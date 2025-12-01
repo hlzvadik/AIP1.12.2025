340e72c6475d4f91214208d940ace5ece44bc7c4
vadik <vadik@debian>
2025-12-01 17:16:13 +0300

    Did for point

--- main.cpp
+++ main.cpp
@@ -3,30 +3,52 @@
 namespace top
 {
 
-struct p_t
-{
-  int x, y;
-};
+  struct p_t
+  {
+    int x, y;
+  };
 
-struct IDrow
-{
-  virtual p_t begin() = 0;
-  virtual p_t next(p_t) = 0;
-};
+  struct IDrow
+  {
+    virtual p_t begin() const = 0;
+    virtual p_t next(p_t) const = 0;
+  };
 
-bool operator==(p_t a, p_t b)
-{
-  return a.x == b.x && a.y == b.y;
-}
+  bool operator==(p_t a, p_t b)
+  {
+    return a.x == b.x && a.y == b.y;
+  }
 
-bool operator!=(p_t a, p_t b)
-{
-  return !(a == b);
-}
+  bool operator!=(p_t a, p_t b)
+  {
+    return !(a == b);
+  }
 
+  struct Dot: IDrow
+  {
+    p_t begin() const override;
+    p_t next(p_t) const override;
+    p_t o;
+    Dot(int x, int y);
+  };
 }
 
 int main()
 {
   using namespace top;
+}
+
+top::Dot::Dot(int x, int y):
+  IDrow(),
+  o{x,y}
+{}
+
+top::p_t top::Dot::begin() const
+{
+  return o;
+}
+
+top::p_t top::Dot::next(p_t) const
+{
+  return begin();
 }
