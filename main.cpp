39ae8bd00f402c02da8876c294a090e83c9c6698
vadik <vadik@debian>
2025-12-01 16:59:48 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+namespace top
+{
+
+struct p_t
+{
+  int x, y;
+};
+
+struct IDrow
+{
+  virtual p_t begin() = 0;
+  virtual p_t next(p_t) = 0;
+};
+
+bool operator==(p_t a, p_t b)
+{
+  return a.x == b.x && a.y == b.y;
+}
+
+bool operator!=(p_t a, p_t b)
+{
+  return !(a == b);
+}
+
+}
+
+int main()
+{
+  using namespace top;
+}
