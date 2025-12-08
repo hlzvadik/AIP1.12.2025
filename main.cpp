d50d986949c3642e341a349e037e629f224c1036
vadik <vadik@debian>
2025-12-08 16:51:50 +0300

    frame

--- main.cpp
+++ main.cpp
@@ -127,3 +127,24 @@ size_t topit::points(const IDraw& d, p_t** pts, size_t s)
   }
   return delta;  
 }
+
+topit::f_t topit::frame(const p_t* pts, size_t s)
+{
+  if (!s)
+  {
+    throw std::logic_error("Bad size");
+  }
+  int minx = pts[0].x, maxx = minx;
+  int miny = pts[0].y, maxy = miny;
+  for (size_t i = 1; i < s; ++i)
+  {
+    minx = std::min(minx, pts[i].x);
+    maxx = std::max(maxx, pts[i].x);
+    miny = std::min(miny, pts[i].y);
+    maxy = std::maxy(maxy, pts[i].y);
+  }
+  p_t aa{minx, miny};
+  p_t bb{maxx, maxy};
+  return {aa, bb};
+}
+
